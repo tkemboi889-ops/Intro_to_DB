7b83be287f1ac437418c20350de6d3bf044646f8
timothy kemboi <tkemboi889@gmail.com>
2025-10-17 12:27:31 +0300

    add sql statements into the repository

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,18 +1,13 @@
--- alx_book_store.sql
--- Database creation script for the ALX Book Store project
--- Author: Timothy Kemboi
 
--- 1. Create Database
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
--- 2. Authors Table
+
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- 3. Books Table
 CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
@@ -22,7 +17,7 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- 4. Customers Table
+
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
@@ -30,7 +25,7 @@ CREATE TABLE Customers (
     address TEXT
 );
 
--- 5. Orders Table
+
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
@@ -38,7 +33,7 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- 6. Order_Details Table
+
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,

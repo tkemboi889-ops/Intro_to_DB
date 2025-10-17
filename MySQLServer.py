db56633a99635d14da18abc2b2c98c3b82cb603a
timothy kemboi <tkemboi889@gmail.com>
2025-10-17 15:53:15 +0300

    add mysqlserver python script

--- MySQLServer.py
+++ MySQLServer.py
@@ -17,8 +17,8 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
+    except mysql.connector.Error as err:
+        print(f"Error while connecting to MySQL: {err}")
 
     finally:
         # Ensure resources are closed properly

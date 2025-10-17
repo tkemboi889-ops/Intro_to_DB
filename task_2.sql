c0367fd8ab00db933f4edf0e850f02aa976eee7d
timothy kemboi <tkemboi889@gmail.com>
2025-10-17 17:03:46 +0300

    add sql table statements into repository

--- task_2.sql
+++ task_2.sql
@@ -34,15 +34,15 @@ CREATE TABLE IF NOT EXISTS Customers (
 
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT FOREIGN KEY
+    customer_id INT FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
     order_date DATE
 );
 
 
 CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT FOREIGN KEY
-    book_id INT FOREIGN KEY
+    order_id INT FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    book_id INT FOREIGN KEY (book_id) REFERENCES Books(book_id)
     quantity DOUBLE
     
 );
